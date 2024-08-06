941a7e8a24aa540c8b51f97083cc3f508f18c9f4
kacper-echaust <k9echaust@gmail.com>
2024-08-06 20:17:52 +0200

    initialize header

---
+++ src/Components/FAQSection/FAQSection.tsx
@@ -0,0 +1,11 @@
+import { FAQHeader } from '../FAQHeader/FAQHeader'
+
+const FAQSection = () => {
+	return (
+		<div className='bg-white rounded-2xl w-4/6 h-4/6'>
+			<FAQHeader />
+		</div>
+	)
+}
+
+export { FAQSection }
