30b193aa9e6ae2058dd61fb54818d9fd4648ad08
kacper-echaust <k9echaust@gmail.com>
2024-08-06 20:32:04 +0200

    add image

--- src/Components/FAQSection/FAQSection.tsx
+++ src/Components/FAQSection/FAQSection.tsx
@@ -2,7 +2,13 @@ import { FAQHeader } from '../FAQHeader/FAQHeader'
 
 const FAQSection = () => {
 	return (
-		<div className='bg-white rounded-2xl w-4/6 h-4/6'>
+		<div className='flex items-center bg-white rounded-2xl w-5/6 h-4/6 relative'>
+			<div className='w-1/2 '></div>
+			<img
+				src='public\images\thinking-men.png'
+				alt='men who thinking'
+				className='w-1/2 object-contain object-center absolute -left-16'
+			/>
 			<FAQHeader />
 		</div>
 	)
